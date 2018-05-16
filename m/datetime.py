ee3fbd2fd710cf8376d450b149866af5edbfca85
manish kumar singh <39149566+manish25297@users.noreply.github.com>
2018-05-16 00:32:03 -0700

    Add files via upload

---
+++ m/datetime.py
@@ -0,0 +1,10 @@
+from datetime import datetime as d
+
+
+
+print d.now()
+
+print str(d.now()).split(" ")[0]
+print str(d.now()).split(" ")[1]
+print str(d.now()).split(" ")[2]
+
---
+++ m/filehandling15.py
@@ -0,0 +1,4 @@
+file.open("/home/prashant/folder/test_file.txt","w")
+file.write("some string for demo purpode")
+file.write("another line for demo again")
+file.close()
